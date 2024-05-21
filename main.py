8a72b110ea7451303816086ea1d756d319e81dff
BartlomiejWlach423 <bartekwlach423@gmail.com>
2024-05-21 20:37:48 +0200

    finished

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from flask import render_template, make_response, request
-
-from keras.models import load_model, Sequential
 import numpy as np
 
-
-app = Flask(__name)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('hello.html', name=None)
+    return render_template('index.html', name=None)
 
 @app.route('/process',methods=['POST'])
 def process_form():
@@ -17,7 +14,14 @@ def process_form():
     temp_max = float(request.form.get('temp_max'))
     temp_min = float(request.form.get('temp_min'))
     wind = float(request.form.get('wind'))
-    model = load_model('weather_prediction_model.h5')
+    import pickle
+    with open('weather_prediction_model.pkl', 'rb') as f:
+        model = pickle.load(f)
     X_new = np.array([precipitation, temp_max,temp_min,wind]).reshape(1, -1)
     weather_predict = model.predict(X_new)
-    return 'Weather forcast: '+str(weather_predict[0])
+    print(weather_predict)
+    return render_template('result.html', prediction=str(weather_predict[0]))
+
+@app.route('/returnToHome',methods=['POST'])
+def returnToHomePage():
+    return index()
