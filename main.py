0d3abfa5aed2701d117ed992f4060093c20d7670
matsat123 <mateusz.05@onet.pl>
2024-05-21 10:44:36 +0200

    1.0

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from flask import Flask,render_template, make_response, request
-app = Flask(__name__)
+from flask import Flask
+from flask import rendertemplate, makeresponse, request
+
+from keras.models import load_model, Sequential
+import numpy as np
+
+
+app = Flask(__name)
 
 @app.route('/')
 def index():
@@ -7,8 +13,11 @@ def index():
 
 @app.route('/process',methods=['POST'])
 def process_form():
-    precipitation = request.form.get('precipitation')
-    temp_max = request.form.get('temp_max')
-    temp_min = request.form.get('temp_min')
-    wind = request.form.get('wind')
-    return 'Form processed'+precipitation
+    precipitation = float(request.form.get('precipitation'))
+    temp_max = float(request.form.get('temp_max'))
+    temp_min = float(request.form.get('temp_min'))
+    wind = float(request.form.get('wind'))
+    model = load_model('weather_prediction_model.h5')
+    X_new = np.array([precipitation, temp_max,temp_min,wind]).reshape(1, -1)
+    weather_predict = model.predict(X_new)
+    return 'Weather forcast: '+str(weather_predict[0])
