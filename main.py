658178609d12913e05dbb033f9a453cf0cbf6a61
matsat123 <mateusz.05@onet.pl>
2024-05-23 11:38:35 +0200

    1.3
    Minior changes

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ def process_form():
     return render_template('index.html')
 
 @app.errorhandler(404)
-def something_went_wrong(error):
-    return render_template()
+def page_not_found(error):
+    return render_template('404.html',error = error)
